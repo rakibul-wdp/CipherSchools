a3fd90d4355b1dd450b45cbf485f1177325a8a76
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-23 04:55:00 +0600

    create ultimate  app component

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Loader from '@shared/loader';
+import { toast } from 'react-toastify';
+import RequireAuth from '@helpers/requireAuth';
+import { Route, Routes } from 'react-router-dom';
+import { StoreContext } from '@contexts/storeProvider';
+import useGetNotifications from '@servers/useGetNotifications';
+import { Home, NotFound, Signin, Signup, Upload, Watch } from '@pages';
+import React, { Suspense, useContext, useEffect, useState } from 'react';
 
-function App() {
+export default function App() {
+  const [current, setCurrent] = useState(0);
+  const { user } = useContext(StoreContext);
+  const { data } = useGetNotifications({ enabled: !!user?._id });
+
+  function getRevised(data) {
+    return data?.filter(item => item?.isRead === false);
+  }
+
+  useEffect(() => {
+    const diff = data?.data?.length - current;
+    if (diff > 0) {
+      const title = getRevised(data?.data)?.reverse()?.[0]?.title;
+      toast.info(title, {
+        position: 'bottom-left',
+      });
+      setCurrent(data?.data?.length);
+    }
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path='/' element={<RequireAuth then={<Home />} />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/watch/:vId' element={<RequireAuth then={<Watch />} />} />
+          {user?.role === 'creator' ? <Route path='/upload' element={<RequireAuth then={<Upload />} />} /> : <></>}
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </React.Fragment>
   );
 }
-
-export default App;
