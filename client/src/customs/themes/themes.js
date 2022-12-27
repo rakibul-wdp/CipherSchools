3b6f009ecf09d478482217fcd7813946b7813fad
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-28 05:55:00 +0600

    create theme component

---
+++ client/src/customs/themes/themes.js
@@ -0,0 +1,54 @@
+const themes = {
+  white: {
+    name: 'white',
+    text: {
+      main: {
+        head: '#1a1a1a',
+        title: '#333333',
+        para: '#4d4d4d',
+        hold: '#808080',
+      },
+      sub: {
+        head: '#fafafa',
+        title: '#ebebeb',
+        para: '#d9d9d9',
+        hold: '#808080',
+      },
+    },
+    back: {
+      accent: '#f08080',
+      card1: '#fdeaea',
+      card2: '#fad5d5',
+      brdr: '#cccccc',
+      main: '#ffffff',
+      sub: '#f5f7f9',
+    },
+  },
+  black: {
+    name: 'black',
+    text: {
+      main: {
+        head: '#fafafa',
+        title: '#ebebeb',
+        para: '#d9d9d9',
+        hold: '#808080',
+      },
+      sub: {
+        head: '#1a1a1a',
+        title: '#333333',
+        para: '#4d4d4d',
+        hold: '#808080',
+      },
+    },
+    back: {
+      accent: '#556b2f',
+      card1: '#0e1208',
+      card2: '#1c2410',
+      brdr: '#606060',
+      main: '#0e1208',
+      sub: '#1c2410',
+    },
+  },
+};
+
+export default themes;
