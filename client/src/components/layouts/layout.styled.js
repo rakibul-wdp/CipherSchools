1c57fcdb3cdfe70895fc6eeb42f720a30913fa06
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-29 04:25:00 +0600

    add layout styles

---
+++ client/src/components/layouts/layout.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const LayoutContainer = styled.main`
+   min-height: 100vh;
+   width: 100%;
+`;
+
+export const LayoutContent = styled.div`
+   max-width: 1500px;
+   margin: 0 auto;
+`;
