80c39a35782c3564cdc573bbb09ec8ef1db23cda
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-06 05:25:00 +0600

    add loader style

---
+++ client/src/components/shared/loader.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const LoaderContainer = styled.section`
+   height: 100vh;
+   width: 100%;
+   overflow: hidden;
+   display: flex;
+   flex-direction: column;
+   gap: 2rem;
+   align-items: center;
+   justify-content: center;
+   z-index: 100;
+   background-color: #ffffff;
+`;
