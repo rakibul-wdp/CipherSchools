86b0146228c08ba96b7c6381a36dac2773d4e9e3
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-06 05:45:00 +0600

    add logo style

---
+++ client/src/components/shared/logo.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const LogoContainer = styled.h1`
+   display: inline-flex;
+   align-items: center;
+   font-size: 3.6rem;
+   font-weight: 700;
+   color: rgba(0, 0, 0, 0.95);
+   text-transform: capitalize;
+   user-select: none;
+   cursor: pointer;
+
+   span {
+      color: #f08080;
+      line-height: inherit;
+      font-size: inherit;
+      font-weight: inherit;
+   }
+`;
