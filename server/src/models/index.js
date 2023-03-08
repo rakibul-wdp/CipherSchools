fb4b8abcf3cc86eb4f355b34331707ea0a4751a4
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-09 05:05:00 +0600

    require all models

---
+++ server/src/models/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+   users: require('./users'),
+   videos: require('./videos'),
+   comments: require('./comments'),
+   baseComments: require('./baseComments'),
+   notifications: require('./notifications'),
+};
