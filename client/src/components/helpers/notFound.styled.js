344461875a4932eed63ac948bd3e8934772c562f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-11 05:28:17 +0600

    create notFound page style

---
+++ client/src/components/helpers/notFound.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const NotFoundContainer = styled.section`
+   width: 100%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   text-align: center;
+   padding: 2rem 1rem;
+   padding-top: 10rem;
+
+   h1 {
+      font-size: 10rem;
+      color: #f08080;
+   }
+
+   h3 {
+      font-size: 3rem;
+      color: rgba(0, 0, 0, 0.75);
+   }
+`;
