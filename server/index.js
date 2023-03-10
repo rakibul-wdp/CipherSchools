6502d9e46d4edab40f793e511bb4e4431ef18260
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-10 09:59:23 +0600

    create main index file for server and setup it

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log("Hell Video web app");
+require('colors');
+require('dotenv').config();
+const cors = require('cors');
+require('./configs/pathAlias')();
+const routes = require('@routes');
+const express = require('express');
+const databases = require('@databases');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors({ origin: '*' }));
+app.use('/', routes(port));
+
+app.listen(port, () => {
+    console.log(`App is running on port: ${port}`.blue.bold);
+    databases.mongoDB();
+});
