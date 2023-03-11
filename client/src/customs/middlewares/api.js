dabbee2d4bb9071ef68a9f3ff66f865c98f7c65d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-03-11 18:55:57 +0600

    change title

--- client/src/customs/middlewares/api.js
+++ client/src/customs/middlewares/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://pmsvidplay.onrender.com/api/v1',
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 api.interceptors.request.use(
